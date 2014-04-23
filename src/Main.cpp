37060cea8374116e682d9b7d39983af81495a099
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2014-04-23 16:12:25 +0200

    Working version of udp based sonar

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,14 +5,13 @@
 const int baudrate = 115200;
 const int udpPort = 4000;
 
-
 class UDPDriver : public iodrivers_base::Driver {
     public:
     UDPDriver() : Driver(1024){
     
     }
-    bool init(int port){
-        openUDP(std::string(""),port);
+    bool init(std::string udpServer, int port){
+        openUDP(udpServer,port);
         return isValid();
     }
 
@@ -27,7 +26,13 @@ class UDPDriver : public iodrivers_base::Driver {
         }
         s << "\n";
         size_t len = s.str().size();
-        writePacket((const uint8_t*)s.str().c_str(),len);
+        if(!isValid()){
+            std::cout << len << " " << s.str() << std::endl;
+            std::cout << "Warning socket is no open (yet?)" << std::endl;
+        }else{
+            std::cout << len << " " << s.str() << std::endl;
+            writePacket((const uint8_t*)s.str().c_str(),len);
+        }
     }
 
     private:
@@ -43,37 +48,39 @@ int main(int argc, char** argv)
 {
     sea_net::Micron driver;
     UDPDriver udp;
-    if(argc!= 2){
-        std::cerr << "Please call this application with the corresponding device given" << std::endl;
+    if(argc != 3 && argc != 2){
+        std::cerr << "Please call this application with the corresponding device given and the udp server to connect" << std::endl;
+        std::cerr << "The remote server is optional" << std::endl;
         return -1;
     }
 
     sea_net::MicronConfig config;
     config.max_distance = 30;
     config.gain = 0.4;
-
-    udp.init(udpPort);
+    
+    if(argc == 3){
+        if(!udp.init(std::string(argv[2]),udpPort)){
+            std::cerr << "Could not open UDP server" << std::endl;
+        }
+    }
 
     driver.openSerial(argv[1],baudrate);
     base::samples::RigidBodyState rbs;
     driver.configure(config,1000);
     driver.start();
-    //driver.waitForPacket(sea_net::mtHeadData,1000);
     sea_net::PacketType packet_type = sea_net::mtNull;
     base::samples::SonarBeam sonar_beam;
     while(1){
-        while(packet_type != sea_net::mtHeadData){
-            packet_type = driver.readPacket(1000);
-            switch(packet_type){
-            case sea_net::mtHeadData:
-                {
-                     driver.decodeSonarBeam(sonar_beam);
-                     udp.sendSonarBeam(sonar_beam);
-                     break;
-                }
-            default:
-                break;
+        packet_type = driver.readPacket(1000);
+        switch(packet_type){
+        case sea_net::mtHeadData:
+            {
+                 driver.decodeSonarBeam(sonar_beam);
+                 udp.sendSonarBeam(sonar_beam);
+                 break;
             }
+        default:
+            break;
         }
     }
     return 0;
