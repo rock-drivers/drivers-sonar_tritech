bb1e9592e63b1fbb7417b1c0458a1ef734400108
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2015-01-18 16:37:30 -0200

    attempt to fix seaking configuration
    This commit is the result of battling with the driver to get any
    kind of meaningful data out of the seaking for longish-range targets
    (i.e. more than 2-3 meters).
    
    To do so, I sniffed the communication between the device and the
    tritech windows application, and then compared with what the driver
    was doing. It was mostly identical, but not quite:
     - the 'gain' parameter was obviously a really bad name. I say
       obviously as the name in the configuration message in the driver
       itself was already ad_threshold, which is really what it is.
     - the filt_gain parameter is a complete unknown. The windows application
       does not allow to change it, so make it fixed in the code as well
       until (if ever) it gets properly documented
     - the windows application uses a different type of message to change
       the gain (the real analog gain this time) and the threshold, which
       does not seem to reset the device (i.e. is a lot less costly). Add
       support for this in the driver.
     - the lockout time was not computed properly
     - the driver was not giving access to the acquisition mode. In
       environments with high backscatter and/or when targetting long
       ranges, the default of return-1st-ping is obviously bad.

--- src/SeaNetProfiling.cpp
+++ src/SeaNetProfiling.cpp
@@ -35,38 +35,34 @@ Profiling::~Profiling()
     delete timestamp_estimator;
 }
 
+struct ProfilingHeadCommand
+{
+    uint8_t V3B_params; uint16_t head_control; uint8_t head_type;
+    uint32_t txn_ch1; uint32_t txn_ch2; uint32_t rxn_ch1; uint32_t rxn_ch2;
+    uint16_t pulse_length; uint16_t range_scale; uint16_t left_limit;
+    uint16_t right_limit; uint8_t ad_threshold; uint8_t filt_gain; uint8_t adaptive_gain_control_max;
+    uint8_t adaptive_gain_control_set_point; uint16_t slope_ch1; uint16_t slope_ch2; uint8_t motor_step_delay_time;
+    uint8_t motor_step_angle_size; uint16_t scan_time; uint16_t prf_spl;
+    uint16_t prf_ctl2; uint16_t lockout_time; uint16_t minor_axis_dir;
+    uint8_t major_axis_pan; uint8_t crtl2; uint16_t scan_z; uint8_t ad_threshold_ch1;
+    uint8_t ad_threshold_ch2; uint8_t filt_gain_ch1; uint8_t filt_gain_ch2; uint8_t agc_max_ch1;
+    uint8_t agc_max_ch2; uint8_t agc_set_point_ch1; uint8_t agc_set_point_ch2;
+    uint16_t advanced_slope_ch1; uint16_t advanced_slope_ch2; uint16_t advanced_slope_delay_ch1;
+    uint16_t advanced_slope_delay_ch2;
+
+    ProfilingHeadCommand()
+    {
+        //clear everything 
+        memset(this,0,sizeof(*this));
+    }
+} __attribute__ ((packed));
+
 void Profiling::configure(const ProfilingConfig& config, uint32_t timeout)
 {
-    //some conversions
-    uint16_t left_limit = (((M_PI-config.left_limit.rad)/(M_PI*2.0))*6399.0);
-    uint16_t right_limit = (((M_PI-config.right_limit.rad)/(M_PI*2.0))*6399.0);
-    uint8_t motor_step_angle_size = config.angular_resolution.rad/(M_PI*2.0)*6399.0;
-    uint8_t ad_treshold = config.gain*210;
-    uint8_t filt_gain = config.filt_gain*250;
-    uint32_t freq_chan1_tx = (config.frequency_chan1 * 4294967296) / 32e6;
-    uint32_t freq_chan1_rx = ((config.frequency_chan1 + 455000) * 4294967296) / 32e6;
-    uint32_t freq_chan2_tx = (config.frequency_chan2 * 4294967296) / 32e6;
-    uint32_t freq_chan2_rx = ((config.frequency_chan2 + 455000) * 4294967296) / 32e6;
-    uint16_t slope_ch1 = 114 + ((config.frequency_chan1 - 600000) / 600000) * 36;
-    uint16_t slope_ch2 = 114 + ((config.frequency_chan2 - 600000) / 600000) * 36;
-    uint16_t pulse_length = (config.max_distance + 10) * 25 / 10;
-    uint16_t range_scale = (uint16_t)(config.max_distance * 10.0) & 0x3FFF;
-    uint16_t lockout_time = 542 - ((config.frequency_chan1 - 600000) / 600000) * 271;
-    uint16_t head_control = PRF_FIRST | HASMOT | PRF_MASTER;
-    if(!config.continous)
-        head_control = head_control | PRF_ALT;
-    if(config.select_channel == 2)
-        head_control = head_control | CHAN2;
-    
-    LOG_INFO_S << "Configure Sonar";
-    LOG_DEBUG_S << " right_limit:" << right_limit << " motor_step_angle_size:" << (int)motor_step_angle_size << " initial_gain:"
-                << " lockout_time:" << lockout_time;
-
     //check configuration 
-    if(config.gain < 0.0 || config.gain > 1.0 ||
-       config.angular_resolution.rad < 0 || config.angular_resolution.rad / (0.05625/180.0*M_PI) > 255.0 ||
+    if(config.angular_resolution.rad < 0 || config.angular_resolution.rad / (0.05625/180.0*M_PI) > 255.0 ||
         config.frequency_chan1 < 600000 || config.frequency_chan1 > 1200000 || config.frequency_chan2 < 600000 || config.frequency_chan2 > 1200000 ||
-        config.filt_gain < 0.0 || config.filt_gain > 1.0 || config.select_channel < 1 || config.select_channel > 2 || config.max_distance > 16383.0 || config.max_distance < 0.0)
+        config.select_channel < 1 || config.select_channel > 2 || config.max_distance > 16383.0 || config.max_distance < 0.0)
     {
         throw std::runtime_error("Profiling::Profiling: invalid configuration.");
     }
@@ -80,44 +76,72 @@ void Profiling::configure(const ProfilingConfig& config, uint32_t timeout)
         throw std::runtime_error("Profiling::Profiling: invalid configuration. To many samples per message. Decrease the opening angle or increase the angular resolution.");
     }
 
+    //some conversions
+    uint16_t slope_ch1 = 114 + ((config.frequency_chan1 - 600000) / 600000) * 36;
+    uint16_t slope_ch2 = 114 + ((config.frequency_chan2 - 600000) / 600000) * 36;
+
+    uint16_t head_control = HASMOT | PRF_MASTER;
+    if(!config.continous)
+        head_control |= PRF_ALT;
+    if(config.select_channel == 2)
+        head_control |= CHAN2;
+    if(config.mode == PROFILING_FIRST)
+        head_control |= PRF_FIRST;
+
     speed_of_sound = config.speed_of_sound;
 
+    ProfilingHeadCommand profiling_head_config;
     //generate head data
     profiling_head_config.V3B_params = 0x1D;
+
     profiling_head_config.head_control = head_control;
     profiling_head_config.head_type = 5;
-    profiling_head_config.txn_ch1 = freq_chan1_tx;
-    profiling_head_config.txn_ch2 = freq_chan2_tx;
-    profiling_head_config.rxn_ch1 = freq_chan1_rx;
-    profiling_head_config.rxn_ch2 = freq_chan2_rx;
-    profiling_head_config.pulse_length = pulse_length;
-    profiling_head_config.range_scale = range_scale;
-    profiling_head_config.left_limit = left_limit; 
-    profiling_head_config.right_limit = right_limit;
-    profiling_head_config.ad_threshold = ad_treshold;
-    profiling_head_config.filt_gain = filt_gain;
-    profiling_head_config.adaptive_gain_control_max = 107;
-    profiling_head_config.adaptive_gain_control_set_point = 100;
-    profiling_head_config.slope_ch1 = slope_ch1;
-    profiling_head_config.slope_ch2 = slope_ch2;
-    profiling_head_config.motor_step_delay_time = 25; // 25 to 40
-    profiling_head_config.motor_step_angle_size = motor_step_angle_size;
-    profiling_head_config.scan_time = 30;
+    profiling_head_config.prf_ctl2 = (config.mode == PROFILING_MAX) ? 1 : 0;
+    profiling_head_config.crtl2 = 0;
     profiling_head_config.prf_spl = 0;
-    profiling_head_config.prf_ctl2 = 0;
-    profiling_head_config.lockout_time = lockout_time;
     profiling_head_config.minor_axis_dir = 1600;
     profiling_head_config.major_axis_pan = 1;
-    profiling_head_config.crtl2 = 0;
     profiling_head_config.scan_z = 0;
-    profiling_head_config.ad_threshold_ch1 = 0x32;
-    profiling_head_config.ad_threshold_ch2 = 0x32;
+
+    profiling_head_config.txn_ch1 = (config.frequency_chan1 * 4294967296) / 32e6;
+    profiling_head_config.txn_ch2 = (config.frequency_chan2 * 4294967296) / 32e6;
+    profiling_head_config.rxn_ch1 = ((config.frequency_chan1 + 455000) * 4294967296) / 32e6;
+    profiling_head_config.rxn_ch2 = ((config.frequency_chan2 + 455000) * 4294967296) / 32e6;
+    uint16_t left_limit = (((M_PI-config.left_limit.rad)/(M_PI*2.0))*6399.0);
+    profiling_head_config.left_limit = left_limit; 
+    uint16_t right_limit = (((M_PI-config.right_limit.rad)/(M_PI*2.0))*6399.0);
+    profiling_head_config.right_limit = right_limit;
+    // Value obtained by reverse engineering the communication between the
+    // windows application and the seaking ...
+    profiling_head_config.lockout_time = config.min_distance * 1356;
+    profiling_head_config.pulse_length = 25 + config.max_distance * 25 / 10;
+    profiling_head_config.range_scale = config.max_distance * 10;
+    // Magic number I also got from reverse engineering ... I've picked 13.56
+    // because it is really close to what the windows app does *and* is the same
+    // than the 1356 value for lockout_time which is also a time-to-distance
+    // conversion. Or how to invent correlations where there is probably none.
+    profiling_head_config.scan_time = 13.56 * config.max_distance;
+
+    profiling_head_config.motor_step_delay_time = 25; // 25 to 40
+    uint8_t motor_step_angle_size = config.angular_resolution.rad/(M_PI*2.0)*6399.0;
+    profiling_head_config.motor_step_angle_size = motor_step_angle_size;
+
+    profiling_head_config.ad_threshold = 50;
+    profiling_head_config.ad_threshold_ch1 = 50;
+    profiling_head_config.ad_threshold_ch2 = 50;
+    profiling_head_config.filt_gain = (config.select_channel == 1) ? 1 : 20;
     profiling_head_config.filt_gain_ch1 = 1;
-    profiling_head_config.filt_gain_ch2 = 1;
+    profiling_head_config.filt_gain_ch2 = 20;
+
+    profiling_head_config.adaptive_gain_control_max = 107;
+    profiling_head_config.adaptive_gain_control_set_point = 100;
     profiling_head_config.agc_max_ch1 = 0x6b;
     profiling_head_config.agc_max_ch2 = 0x6b;
     profiling_head_config.agc_set_point_ch1 = 0x64;
     profiling_head_config.agc_set_point_ch2 = 0x64;
+
+    profiling_head_config.slope_ch1 = slope_ch1;
+    profiling_head_config.slope_ch2 = slope_ch2;
     profiling_head_config.advanced_slope_ch1 = 110;
     profiling_head_config.advanced_slope_ch2 = 150;
     profiling_head_config.advanced_slope_delay_ch1 = 0;
@@ -125,6 +149,60 @@ void Profiling::configure(const ProfilingConfig& config, uint32_t timeout)
     
     HeadCommand* head_config = reinterpret_cast<HeadCommand*>(&profiling_head_config);
     writeHeadCommand(*head_config, timeout);
+}
+
+// Change acquisition parameters without resetting the head (fast parameter
+// change)
+//
+// This is purely coming from reverse engineering
+//
+// The 'magic' fields are the fields for which I had no idea what to do
+struct ProfilingAcquisitionConfigMessage
+{
+    // Always 30 for this message
+    uint8_t V3B_params;
+    // A/D thresholds for channels 1 and 2
+    uint8_t ad_threshold[2];
+    // "filt_gain" (no idea what the hell it is). I leave it to the default
+    // values, as even the windows application does not touch it
+    uint8_t filt_gain[2];
+    // The actual gain. Note that this is THE value that - it seems - cannot be
+    // changed in the head profiling configuration structure
+    uint8_t gain[2];
+    // Wild guess. Left at default values of 189
+    uint8_t agc_setpoint[2];
+    // Wild guess. Left at default values of 110 and 150
+    uint16_t slope[2];
+    // Wild guess. Left at default values of 0
+    uint16_t advanced_slope_delay[2];
+} __attribute__ ((packed));
+
+void Profiling::configureAcquisition(ProfilingAcquisitionConfig const& config, uint32_t timeout)
+{
+    if (has_pending_data)
+        throw std::runtime_error("requestData() called and the corresponding receiveData() has not been called");
+
+    ProfilingAcquisitionConfigMessage msg;
+    msg.V3B_params = 30;
+    msg.ad_threshold[0] = msg.ad_threshold[1] = config.ad_threshold * 255;
+    msg.filt_gain[0] = 1;
+    msg.filt_gain[1] = 20;
+    msg.gain[0] = config.gain * 255;
+    msg.gain[1] = config.gain * 255;
+    msg.agc_setpoint[0] = msg.agc_setpoint[1] = 189;
+    msg.slope[0] = 110;
+    msg.slope[1] = 150;
+    msg.advanced_slope_delay[0] = 0;
+    msg.advanced_slope_delay[1] = 0;
+
+    std::vector<uint8_t> packet =
+        SeaNetPacket::createPaket(device_type,
+                mtHeadCommand,
+                (uint8_t*)&msg,
+                sizeof(msg));
+
+    LOG_DEBUG_S <<"Sent mtHeadCommand packet" ;
+    writePacket(&packet[0],packet.size(),timeout);
 }
 
 void Profiling::decodeScan(base::samples::LaserScan& scan)
--- src/SeaNetTypes.hpp
+++ src/SeaNetTypes.hpp
@@ -35,42 +35,78 @@ namespace sea_net
         mtSendPreferenceData
     };
 
+    /** Acquisition mode for the profiling sonar
+     *
+     * This governs what information the sonar returns
+     */
+    enum PROFILING_MODE
+    {
+        /** Return the first echo that is above the ad_threshold parameter
+         *
+         * This is the fastest mode (as the head can continue after the first
+         * echo), but is obviously dependent on choosing both a good gain and
+         * threshold
+         */
+        PROFILING_FIRST,
+        PROFILING_NORMAL,
+        /** Return the highest echo */
+        PROFILING_MAX
+    };
+
+    /** Complete configuration of the SeaKing
+     *
+     * Note that ProfilingAcquisitionConfig allows to set a subset of the
+     * parameters in a much faster way
+     */
     struct ProfilingConfig
     {
+        PROFILING_MODE mode;
+
         base::Angle left_limit;
         base::Angle right_limit;
         base::Angle angular_resolution; 
 
+        /** Which channel to use */
         unsigned int select_channel;
         unsigned int frequency_chan1;
         unsigned int frequency_chan2;
         
+        /** Maximum distance. A higher distance usually means longer acquisition
+         * times as the sonar has to wait longer for an echo before going on
+         */
         double max_distance;
+        /** Minimum distance under which echoes will be ignored (in m) */
         double min_distance;
+        /** The speed of sound (by default 1500 m/s) used to convert
+         * configuration parameters in meters to device parameters that are
+         * usually in seconds
+         */
         double speed_of_sound;
-        double gain;
-        double filt_gain;
         
+        /** Whether scanning should go always in the same direction or
+         * "ping-pong". When scanning a small segment, a non-continuous mode is
+         * recommended
+         */
         bool continous;
         
         ProfilingConfig() : 
+            mode(PROFILING_FIRST),
             left_limit(base::Angle::fromRad(M_PI)),
             right_limit(base::Angle::fromRad(-M_PI)),
             angular_resolution(base::Angle::fromRad(5.0/180.0*M_PI)),
-            select_channel(1),
+            select_channel(2),
             frequency_chan1(600000),
             frequency_chan2(1200000),
             max_distance(15.0),
             min_distance(1.0),
             speed_of_sound(1500.0),
-            gain(0.2),
-            filt_gain(0.01),
             continous(false)
         {};
       
         bool operator==(const ProfilingConfig &other) const
         {
-            return (other.left_limit == left_limit &&
+            return (other.mode == mode &&
+                    other.left_limit == left_limit &&
                     other.right_limit == right_limit &&
                     other.angular_resolution == angular_resolution &&
                     other.select_channel == select_channel &&
@@ -79,14 +115,37 @@ namespace sea_net
                     other.max_distance == max_distance && 
                     other.min_distance == min_distance && 
                     other.speed_of_sound== speed_of_sound &&
-                    other.gain == gain &&
-                    other.filt_gain == filt_gain);
+                    other.continous == continous);
         };
         
         bool operator!=(const ProfilingConfig &other) const
         {
             return !(other == *this);
         };
+    };
+
+    /** Smaller configuration structure for acquisition parameters that can be
+     * changed live on the Tritech seaking.
+     *
+     * Changing these parameters is almost instantaneous, unlike using the
+     * ProfilingConfig which involves resetting the whole device
+     */
+    struct ProfilingAcquisitionConfig
+    {
+        /** The acquisition gain in [0, 1]. Documentation recommends to start
+         * with 40% (0.4)
+         */
+        double gain;
+        /** Signal under this threshold will be ignored by the sonar. The
+         * documentation recommends to stick with 0.2
+         *
+         * The value is in percent, i.e. [0, 1]
+         */
+        double ad_threshold;
+
+        ProfilingAcquisitionConfig()
+            : gain(0.4)
+            , ad_threshold(0.2) {}
     };
 
     //some values are not working for the micron dst like lock_out time
